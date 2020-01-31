df97c13d81e04d7d475debdedc7957660e5ebdd7
Robustprogram <robustprogram@users.noreply.github.com>
2020-01-31 17:39:41 +1100

    Added ability to route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Video from './components/Video';
 import Menu from './components/Menu';
 
 // Contents
 import Landing from './components/Content/Landing';
+import AboutUs from './components/Content/AboutUs';
 
 import Footer from './components/Footer';
 
@@ -14,8 +16,17 @@ function App() {
   return (
     <React.Fragment>
       <Menu />
-      <Video />
-      <Landing />
+      <Router>
+        <Switch>
+          <Route path="/about-us">
+            <AboutUs />
+          </Route>
+          <Route path="/">
+            <Video />
+            <Landing />
+          </Route>
+        </Switch>
+      </Router>
       <Footer />
     </React.Fragment>
   );
---
+++ src/components/Content/AboutUs/AboutUs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Image1 from '../../../img/aboutus-1.jpg';
+
+function AboutUs() {
+  return (
+    <div id="aboutus">
+      <p className="title">ABOUT US</p>
+      <p>UNSW Med Revue is an annual comedy sketch show, filled with singing, dancing and acting! For the past 43 years, our society has produced high-calibre shows for all at UNSW to enjoy. Whether you’re about getting on stage to make people laugh, programming lights and smoke machines, filming and editing videos, or cooking up great food, we’ve got something for everyone regardless of your degree or abilities. Our portfolio teams cover a wide range of interests so there is something for everyone to get involved with!</p>
+      <p>Beyond the theatre, we’re all about making friends for life, performing hard and partying even harder. We also strive to teach everyone new skills and allow individuals to achieve personal development to be utilised beyond the stage! Get involved with a hallmark event in UNSW’s calendar and become part of one of the largest communities on campus!</p>
+    </div>
+  )
+}
+
+export default AboutUs;
